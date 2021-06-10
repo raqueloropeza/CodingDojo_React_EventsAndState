98171a191c52673be35ce483a274e2e1284705cd
Raquel Oropeza <rockuzaki@gmail.com>
2021-06-10 11:38:40 -0500

    initial commit

---
+++ src/components/PersonCard.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class PersonCard extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            clickage: props.age
+        }
+    }
+    render(){
+        const { lastName, firstName, hairColor} = this.props;
+        const ageUp = () => this.setState({clickage: this.state.clickage +1})
+        return(
+            <div>
+                <h1>{lastName}, {firstName} </h1>
+                <p>Age: {this.state.clickage}</p>
+                <p>Hair Color: {hairColor} </p>
+                <button onClick={ageUp}>Birthday Button for {firstName} {lastName}</button>
+            </div>
+        )
+    }
+}
+
+export default PersonCard;
